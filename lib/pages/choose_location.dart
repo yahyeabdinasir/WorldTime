f67cd64b278b9dbcd96d9491aca1006ad7aa615b
yahye <yahyeabdinasirfarah@gmail.com>
2026-01-24 21:30:04 +0300

    displaying the edit location page and appliying some widget

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time_api.dart';
 
 
 
@@ -11,21 +12,49 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTimeApi> locations = [
+    WorldTimeApi(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTimeApi(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTimeApi(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTimeApi(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTimeApi(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTimeApi(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTimeApi(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTimeApi(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      backgroundColor: Colors.grey[500],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor: Colors.blue[600],
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
         centerTitle: true,
-        title: Text("Location"),
-        // elevation: 0,
+        elevation: 0,
       ),
-
-
-      );
-
-
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
   }
 }
